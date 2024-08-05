d04230cde6596e9a2ac55565d32f2548f6221a47
Nastyya2013 <156695420+Nastyya2013@users.noreply.github.com>
2024-08-05 16:39:48 +0300

    Create styles.cs

---
+++ styles.cs
@@ -0,0 +1,70 @@
+body {
+    font-family: COMIC SANS MS;
+}
+header{
+    background-image: linear-gradient(#5C8BAB, #C6D6E0,#5C8BAB );
+}
+h1{
+   
+   text-align: center;
+   color: white;
+   font-size: 50px;
+   position: relative;
+}
+.img{
+    height:95px;
+    width:135px;
+    position: absolute;
+    top:20px;
+    left: 70px;
+}
+.pp{
+   text-align: center;
+   color: white;
+   font-size: 30px;
+   background-color: #82A8C2;
+   padding: 30px;
+}
+.catalog {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    width: 100%;
+    margin: 0 auto;
+}
+.product {
+    width: 150px;
+    text-align: center;
+    padding: 10px;
+    margin-top: 40px;
+    margin-bottom: 40px;
+    background-color: white;
+    position: relative;
+}
+.product:hover{
+    border: 1px solid black;
+}
+.imm {
+    height: 100px;
+    width: 100px;
+    padding: 20px;
+}
+.immg{
+    height: 120px;
+    width: 120px;
+    padding: 20px;
+}
+a {
+    text-decoration: none;
+    color: black;
+    font-size: 20px;
+}
+p{
+    text-align: center;
+    color: white;
+    font-size: 20px;
+}
+footer{
+    background-color: #82A8C2;
+    padding: 20px;
+}
